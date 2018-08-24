5cf14689f830d458ec1222905fda5ee8d6770f64
InexYk <1959108511@qq.com>
2018-08-24 09:19:48 +0800

    HUC
    HUC blockchain publicity

---
+++ js/auto-size.js
@@ -0,0 +1,13 @@
+﻿(function (doc, win) {
+            var docEl = doc.documentElement,
+                    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+                    recalc = function () {
+                        var clientWidth = docEl.clientWidth;
+                        if (!clientWidth) return;
+						if(clientWidth>640){clientWidth=640;}
+                        docEl.style.fontSize = (24*clientWidth/750) + 'px';
+                    };
+            if (!doc.addEventListener) return;
+            win.addEventListener(resizeEvt, recalc, false);
+            recalc();
+        })(document, window);
